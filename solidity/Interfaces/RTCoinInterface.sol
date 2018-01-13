5b6b39497fabe5da67fde8b7d7bdade988016ad1
postables <postables@none.com>
2018-01-13 00:37:51 -0800

    Added safemath, admin module, and rtc interface

---
+++ solidity/Interfaces/RTCoinInterface.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.19;
+
+/**
+RTCoin Interface
+*/
+
+interface RTCoinInterface {
+    
+    function freezeTransfers() public returns (bool);
+
+    function thawTransfers() public returns (bool);
+
+    function transfer(address _recipient, uint256 _amount) public returns (bool transferred);
+
+    function transferFrom(address _owner, address _recipient, uint256 _amount) public returns (bool transferredFrom);
+
+    function approve(address _spender, uint256 _amount) public returns (bool approved);
+    /**GETTERS */
+
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address _holder) public view returns (uint256);
+
+    function allowance(address _owner, address _spender) public view returns (uint256);
+}
