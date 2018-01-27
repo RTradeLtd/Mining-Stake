1c08f0181eca7925bd2f1b469b420b8220935a73
postables <postables@none.com>
2018-01-26 16:24:52 -0800

    Assertion error that was fixed. Due to testing being done on a PoA network, needed to implement the moneky patch pointed out by dongsam on web3py repo

--- python/stakeUtil.py
+++ python/stakeUtil.py
@@ -2,6 +2,17 @@ from web3 import Web3, IPCProvider
 import web3
 import sys
 
+from web3.middleware.pythonic import (
+    pythonic_middleware,
+    to_hexbytes,
+)
+
+size_extraData_for_poa = 200   # can change
+
+web3 = Web3(IPCProvider('/home/solidity/.ethereum/geth.ipc'))
+pythonic_middleware.__closure__[2].cell_contents['eth_getBlockByNumber'].args[1].args[0]['extraData'] = to_hexbytes(size_extraData_for_poa, variable_length=True)
+pythonic_middleware.__closure__[2].cell_contents['eth_getBlockByHash'].args[1].args[0]['extraData'] = to_hexbytes(size_extraData_for_poa, variable_length=True)
+
 
 
 # python3.6 stakeUtil.py <command>
@@ -24,20 +35,20 @@ if command not in commands:
 
 
 
-def lockupTokens(contract):
+def lockupTokens(contract, w3):
     rtcToStake = int(input('how much RTC do you wish to stake: '))
     timeToStake = int(input('for how many weeks do you wish to stakeL: '))
-    assert rtcToStake > 0 and timeToStake > 4
-    contract.transact({'from': '123'}).lockupRtcTokens(rtcToStake, timeToStake)
+    assert rtcToStake > 0 and timeToStake >= 4
+    contract.transact({'from': w3.eth.accounts[0]}).lockupRtcTokens(rtcToStake, timeToStake)
 
-w3 = Web3(IPCProvider('ipcpath'))
+w3 = Web3(IPCProvider('/home/solidity/.ethereum/geth.ipc'))
 
 # unlock account
-web3.personal.unlockAccount(web3.eth.accounts[0], 'password', 0)
+w3.personal.unlockAccount(w3.eth.accounts[0], 'password123', 0)
 
 # load the contract
-contract = w3.eth.contract('data')
+contract = w3.eth.contract('0x1F60a5B6369179d7533Fc7f721EB8BB3dF0E95ea')
 
 
 if command == commands[0]:
-    lockupTokens(contract)
+    lockupTokens(contract, w3)
