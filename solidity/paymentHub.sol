cd05dca461677957617e67011e83821063b1a49b
postables <postables@none.com>
2018-02-27 23:30:12 -0800

    Updated payment router, slightly decreased gas cost

--- solidity/paymentHub.sol
+++ solidity/paymentHub.sol
@@ -9,51 +9,27 @@ Used to route payments to stake holders
 */
 
 
-contract Payment is Administration {
-
-	ERC20Interface public eI;
-
+contract PaymentRouter is Administration {
 
 	function () payable {}
 
-
-	function setErcInterface(
-		address _tokenAddress)
-		onlyAdmin
-		public
-		returns (bool)
-	{
-		eI = ERC20Interface(_tokenAddress);
-	}
-
 	function routePayments(
 		address[] _recipients,
+		address _tokenAddress,
 		uint256 _tokensToSend,
 		uint256 _ethToSend)
 		public
 		onlyAdmin
-		payable
 		returns (bool)
 	{
+		ERC20Interface eI = ERC20Interface(_tokenAddress);
 		for (uint256 i = 0; i < _recipients.length; i++) {
 			if (_recipients[i] != address(0)) {
 				require(this.balance >= _ethToSend);
-				require(eI.transferFrom(msg.sender, _recipients[i], _tokensToSend));
+				require(eI.transfer(_recipients[i], _tokensToSend));
 				_recipients[i].transfer(_ethToSend);
 			}
 		}
 	}
 
-	function routePayment(
-		address _recipient,
-		uint256 _tokensToSend,
-		uint256 _ethToSend)
-		public
-		onlyAdmin
-		returns (bool)
-	{
-		require(eI.transferFrom(msg.sender, _recipient, _tokensToSend));
-		_recipient.transfer(_ethToSend);
-		return true;
-	}
 }
