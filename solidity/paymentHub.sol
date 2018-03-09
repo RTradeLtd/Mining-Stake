6d16c096feb6e230196089c96609999bec88368d
postables <postables@none.com>
2018-03-09 00:08:31 -0800

    payment router update

--- solidity/paymentHub.sol
+++ solidity/paymentHub.sol
@@ -11,13 +11,15 @@ Used to route payments to stake holders
 
 contract PaymentRouter is Administration {
 
+	event RTCSent(address _recipient, uint256 _amount);
+	event ETHSent(address _recipient, uint256 _amount);
+
 	function () payable {}
 
-	function routePayments(
+	function routeTokenPayments(
 		address[] _recipients,
 		address _tokenAddress,
-		uint256 _tokensToSend,
-		uint256 _ethToSend)
+		uint256 _tokensToSend)
 		public
 		onlyAdmin
 		returns (bool)
@@ -25,8 +27,23 @@ contract PaymentRouter is Administration {
 		ERC20Interface eI = ERC20Interface(_tokenAddress);
 		for (uint256 i = 0; i < _recipients.length; i++) {
 			if (_recipients[i] != address(0)) {
-				require(this.balance >= _ethToSend);
+				RTCSent(_recipients[i], _tokensToSend);
 				require(eI.transfer(_recipients[i], _tokensToSend));
+			}
+		}
+	}
+
+	function routeEthPayments(
+		address[] 	_recipients,
+		uint256 	_ethToSend)
+		public
+		onlyAdmin
+		returns (bool)
+	{
+		for (uint256 i = 0; i < _recipients.length; i++) {
+			if (_recipients[i] != address(0)) {
+				require(this.balance >= _ethToSend);
+				ETHSent(_recipients[i], _ethToSend);
 				require(_recipients[i].send(_ethToSend));
 			}
 		}
